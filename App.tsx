471916a8f8d3aee918daa4a99e906c6f9ee8e259
Blendow Mendes <blendowmendes@gmail.com>
2025-08-19 20:44:59 -0300

    Atividades da primeira aula pratica

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Modal, FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.telaPincipal}>
+      <Button
+        title='Adicionar Item'
+        onPress={() => setModalVisible(true)}
+      />
+
+      <Modal visible={modalVisible}  transparent={true}>
+        <View style={styles.telaModalExterno}>
+          <View style={styles.modalConteudo}>
+            <Text>conteudoModal</Text>
+            <Button title='Fechar' onPress={() => {setModalVisible(false)}}/>
+          </View>
+        </View>
+      </Modal>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  telaPincipal: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  telaModalExterno: {
+    flex: 1,
+    backgroundColor: '#000000c0',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  modalConteudo: {
+    alignItems: 'center',
+    width: 300,
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 8,
+    textAlign: 'center'
+  }
 });
